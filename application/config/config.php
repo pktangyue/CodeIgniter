6a74d1f08002989c5ef3f97368371f40f2082747
tangyue <tangyue1004@gmail.com>
2012-03-13 23:09:08 +0800

    login check complete

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'f4be50f49d25982f4acdd1a98d51b4e0';
 
 /*
 |--------------------------------------------------------------------------
